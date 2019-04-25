9426cb2e4a4a4d7a39a8ed34af3dd19eb03d92a6
guoruibiao <marksinoberg@gmail.com>
2019-04-25 22:35:40 +0800

    天呐，又写错链接了

--- dao/shortdao_test.go
+++ dao/shortdao_test.go
@@ -8,7 +8,7 @@ func Test_SinaURLShort(t *testing.T) {
 	originurl := "https://github.com"
 	dao, _ := New()
 	response, _ := dao.SinaURLShort(originurl)
-	if response.URLS[0].ShortURL == "http://t.cn/RxnlTYR" {
+	if response.URLS[0].ShortURL == "http://t.cn/aktT6M" {
 		t.Log("SinaURLShort passed")
 	} else {
 		t.Error("SinaURLShort failed", originurl, " to ", response.URLS[0].ShortURL)
